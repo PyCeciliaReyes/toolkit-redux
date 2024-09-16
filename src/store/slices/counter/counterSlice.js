3d778d063793fa19c3dd70304bc7a1fd574a59c0
PyCeciliaReyes <cecibreyes@gmail.com>
2024-09-16 15:51:27 -0400

    creacion de mas acciones en el counterSlice y utilizar en App.jsx

--- src/store/slices/counter/counterSlice.js
+++ src/store/slices/counter/counterSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { act } from 'react';
 
 const initialState = {
   value: 0,
@@ -16,10 +17,16 @@ export const counterSlice = createSlice({
       // which detects changes to a "draft state" and produces a brand new
       // immutable state based off those changes
       state.counter += 1;
-    }
+    },
+    decrement: (state) => {
+      state.counter -= 1
+    },
+    incrementBy: (state, action) => {
+      state.counter += action.payload
+    },
   },
 })
 
 // Action creators are generated for each case reducer function
-export const { increment } = counterSlice.actions
+export const { increment, decrement, incrementBy } = counterSlice.actions
 
