a60c50d03cc486fde5ba612b495ec9f5385f1924
PyCeciliaReyes <cecibreyes@gmail.com>
2024-09-17 10:34:17 -0400

    Consumir el API mediante el custom hook: useGetTodosQuery, useGetTodoQuery
    Y obtener un Todo por ID

--- src/TodoApp.jsx
+++ src/TodoApp.jsx
@@ -1,18 +1,51 @@
+import { useState } from "react";
+import { useGetTodoQuery, useGetTodosQuery } from "./store/apis"
 
 export const TodoApp = () => {
+    const [todoId, setTodoId] = useState(1);
+    //const {data: todos = [], isLoading } =useGetTodosQuery();
+    const {data: todo, isLoading } = useGetTodoQuery(todoId);
+
+    const nexTodo = () => {
+        setTodoId( todoId + 1)
+    }
+
+    const PrevTodo = () => {
+        if (todoId === 1) return;
+        setTodoId( todoId - 1)
+    }
+
   return (
     <>
         <h1>Todos - RTK Query</h1>
         <hr />
-        <h4>isLoading...</h4>
+        <h4>isLoading; {isLoading ? 'True' : 'False'}</h4>
 
-        <pre>....</pre>
+        <pre>{JSON.stringify(todo)}</pre>
 
         <button
-        
+            onClick={nexTodo}
         >
             Next Todo
         </button>
+
+        <button
+            onClick={PrevTodo}
+        >
+            Prev Todo
+        </button>
+
+        {/* <ul>
+            {
+                todos.map(todo => (
+                    <li key={todo.id}>
+                        <strong>{todo.completed ? 'Done' : 'Pending'} </strong>
+                        {todo.title}
+                    </li>
+                ))
+            
+            }
+        </ul> */}
     </>
   )
 }
