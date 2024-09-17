e8f5e9f061ac356a23f68cbdf450e2b64ce91d96
PyCeciliaReyes <cecibreyes@gmail.com>
2024-09-17 09:10:35 -0400

    Instalacion del paquete axios
    Configurando la peticion con fetch y con axios

--- src/store/slices/pokemon/thunks.js
+++ src/store/slices/pokemon/thunks.js
@@ -1,12 +1,18 @@
 //todos lo relacionado a pokemon slice para peticiones http
+import { pokemonApi } from '../../../api/pokemonApi';
 import {setPokemons, startLoadingPokemons} from './pokemonSlice';
 
 
 export const getPokemons = (page = 0) => {
     return async (dispatch, getState) => {
         dispatch( startLoadingPokemons());
-        //todo realizar la peticion http
-        //data-pokemons
-        //dispatch(setPokemons(data-pokemons));
+
+        //TODO realizar la peticion http
+        // const resp = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=10&offset=${page * 10}`)
+        // const data = await resp.json();
+        const {data} = await pokemonApi.get(`/pokemon?limit=10&offset=${page * 10}`);
+        //console.log(resp)
+
+        dispatch(setPokemons({pokemons: data.results, page: page + 1 }));
     }
 }
